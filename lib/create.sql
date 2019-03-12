ac444547c701ec3cc83a897a54bb343ba40c4822
Nazib Haque <simply.nazib@gmail.com>
2019-03-12 13:04:24 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,20 +1,21 @@
-
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
-  funding_goal INTEGER,
-  start_date DATE,
-  end_date DATE
-  );
+  funding_goal REAL,
+  start_date TEXT,
+  end_date TEXT
+);
+
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
 );
-CREATE TABLE pledges(
+
+CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
-  amount INTEGER,
+  amount REAL,
   user_id INTEGER REFERENCES user(id),
-  project_id INTEGER REFERENCES projects(id)
-)
+  project_id INTEGER REFERENCES project(id)
+);
